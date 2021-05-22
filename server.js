286c1a4cde49749237d10b49ee30ad2eee149e33
Aryan <aryandtu1@gmail.com>
2021-05-22 15:58:36 +0530

    Adding Swagger Documentation for register api
    Url : http://localhost:4000/api-docs/

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const swaggerUi = require("swagger-ui-express");
+swaggerDocument = require("./swagger.json");
+
 
 const app = express();
 
@@ -8,6 +11,9 @@ app.use(bodyParser.json());
 
 // parse requests of content-type: application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
+
+//Adding swagger related config
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // simple route
 app.get("/", (req, res) => {
