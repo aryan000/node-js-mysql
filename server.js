30919a8c9c82d7b1929d4e6a80370b786d297a26
Aryan <aryandtu1@gmail.com>
2021-05-23 13:59:52 +0530

    Allowing CORS calls

--- server.js
+++ server.js
@@ -14,6 +14,13 @@ app.use(bodyParser.json());
 // parse requests of content-type: application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Allowing cors calls
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 //Adding swagger related config
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
